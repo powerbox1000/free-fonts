e8b87bdf0a470220b21648f0a597ff2e46b0c0e1
powerbox1000 <72870790+powerbox1000@users.noreply.github.com>
2020-11-02 11:44:41 -0500

    Add files via upload

---
+++ scripts/fontHandler.js
@@ -0,0 +1,27 @@
+var characters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 0 ! @ # $ % ^ & * ( ) - _ + = [ ] { } \\ | ; : ' \" . , < > / ? ` ~";
+
+var previewDiv = document.getElementById("previewDiv");
+var fontStyle = document.getElementById("fType");
+var fontName = document.getElementById("fName");
+
+function previewFont(){
+  previewDiv.className = fontName.value;
+  previewDiv.innerText = characters;
+}
+
+function done(){
+  var generated = "https://freefonts.powerbox1000.repl.co/%1/%2".replace("%1",fontStyle.value).replace("%2",fontName.value);
+if(fontStyle.value == "css"){
+  try{
+  var el = document.createElement("button");
+  el.setAttribute("data-clipboard-target","#fName");
+  var clip = new ClipboardJS(el);
+  alert("CSS URL copied to clipboard.");
+  }catch(e){
+    console.warn(e);
+    alert("Error copying CSS URL. The CSS URL is: "+generated);
+  }
+}else{
+  window.open(generated + ".otf");
+}
+}
